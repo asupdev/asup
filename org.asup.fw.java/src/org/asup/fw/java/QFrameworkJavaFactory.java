ac4e529533c2211c9cffa59ae02afd67426b1f9e
Mattia Rocchi <mattiarocchi@gmail.com>
2014-08-19 03:39:46 +0200

    Creazione plugin org.asup.fw.java #2

--- org.asup.fw.java/src/org/asup/fw/java/QFrameworkJavaFactory.java
+++ org.asup.fw.java/src/org/asup/fw/java/QFrameworkJavaFactory.java
@@ -15,7 +15,7 @@ import org.eclipse.emf.ecore.EFactory;
  * It provides a create method for each non-abstract class of the model.
  * <!-- end-user-doc -->
  * @see org.asup.fw.java.QFrameworkJavaPackage
- * @generated
+ * @generated 
  */
 public interface QFrameworkJavaFactory extends EFactory {
 	/**
