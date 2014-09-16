53a6a577a9e427ad88af26d43b461710b7bf8c91
Mattia Rocchi <mattiarocchi@gmail.com>
2014-09-16 17:26:15 +0200

    Gestione GOTO #15

---
+++ org.asup.os.type.pgm.asm/src/org/asup/os/type/pgm/asm/ASMProgramLoader.java
@@ -0,0 +1,48 @@
+package org.asup.os.type.pgm.asm;
+
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.asup.fw.core.impl.ServiceImpl;
+import org.objectweb.asm.ClassAdapter;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Label;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.hooks.weaving.WeavingHook;
+import org.osgi.framework.hooks.weaving.WovenClass;
+import org.osgi.framework.wiring.BundleWiring;
+
+
+public class ASMProgramLoader extends ServiceImpl implements WeavingHook {
+
+	@Override
+	public void weave(WovenClass wovenClass) {
+
+		BundleWiring bundleWiring = wovenClass.getBundleWiring();
+		Bundle bundle = bundleWiring.getBundle();
+		
+		// ASUP project
+		// TODO for performances reason, check a single file not directory
+		if(bundle.getSymbolicName().equals("P_MULT"))
+			completeClass(wovenClass);
+	}
+	
+	private void completeClass(WovenClass wovenClass) {
+        ClassReader cr = new ClassReader(wovenClass.getBytes());
+        
+        LabelAnalyzer labelAnalyzer = new LabelAnalyzer();        
+        cr.accept(labelAnalyzer, 0);
+        
+        Map<String, Label> tag2Label = labelAnalyzer.getTag2Label();
+        for(Entry<String, Label> entry: tag2Label.entrySet()) {
+        	System.out.println(entry.getKey()+"="+entry.getValue()+"("+entry.getValue().info+")");
+        }
+        
+        ClassWriter cw = new ClassWriter(cr, 0);  
+        ClassAdapter ca = new JumpTransformer(cw, wovenClass.getClassName().replace(".", "/"), tag2Label);
+        cr.accept(ca, 0);
+        
+        wovenClass.setBytes(cw.toByteArray());
+	}
+}
