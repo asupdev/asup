ab030cd35b6203bfd1f17a26f86839cca1b1221b
Mattia Rocchi <mattiarocchi@gmail.com>
2014-10-01 12:22:20 +0200

    NIO test encoding

--- org.asup.il.data.nio/src/org/asup/il/data/nio/test/TestHexadecimal.java
+++ org.asup.il.data.nio/src/org/asup/il/data/nio/test/TestHexadecimal.java
@@ -3,44 +3,26 @@ package org.asup.il.data.nio.test;
 import java.io.UnsupportedEncodingException;
 
 import org.asup.il.data.nio.NIOCharacterImpl;
-import org.asup.il.data.nio.NIODecimalImpl;
 
 public class TestHexadecimal {
 	
 	private static String ENCODING = "IBM-280";
 	
 	public static void main(String[] args) throws UnsupportedEncodingException {
+
+		String inputHex = "C1A24BE4D7";
 		
-		int i= Integer.parseInt("40",16);
-		System.out.println("Decimal:="+ i);
-		
-		
-		NIODecimalImpl decimal = new NIODecimalImpl(10, 0, null);
-		decimal.allocate();
-		decimal.eval(64);
+		byte[] bytes = new byte[5];
+		for(int i=0; i<5; i++) {
+			String hex = new String(inputHex.substring(2*i, 2*i+2));
+			int b = Integer.parseInt(hex,16);
+			bytes[i] = (byte) b;
+		}
 
 		NIOCharacterImpl character = new NIOCharacterImpl(15, null);
 		character.allocate();
-		byte[] bytes = new byte[1];
-		bytes[0] = 64;
 		character.movel(new String(bytes, ENCODING));
 		System.out.println(character.toString());
 
-		character.movel(decimal);
-		
-		String inputHex = null;
-		
-		inputHex = "40";
-		System.out.println(fromHexString(inputHex));
-		System.out.println(Integer.parseInt(inputHex, 16));
-	}
-	
-
-	public static String fromHexString(String hex) {
-	    StringBuilder str = new StringBuilder();
-	    for (int i = 0; i < hex.length(); i+=2) {
-	        str.append((char) Integer.parseInt(hex.substring(i, i + 2), 16));
-	    }
-	    return str.toString();
 	}
 }
