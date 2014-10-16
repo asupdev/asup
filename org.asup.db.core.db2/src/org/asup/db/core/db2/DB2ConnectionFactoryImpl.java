7a0a293cb43213b917da0ed5a8e9d19eae462b24
Franco Lombardo <f.lombardo69@gmail.com>
2014-10-16 17:18:26 +0200

    Database DB2 plugin #44

--- org.asup.db.core.db2/src/org/asup/db/core/db2/DB2ConnectionFactoryImpl.java
+++ org.asup.db.core.db2/src/org/asup/db/core/db2/DB2ConnectionFactoryImpl.java
@@ -9,37 +9,66 @@ import javax.sql.DataSource;
 import javax.sql.XADataSource;
 
 import org.asup.db.core.impl.ConnectionFactoryImpl;
+import org.osgi.service.jdbc.DataSourceFactory;
+
+import com.ibm.db2.jcc.*;
 
 public class DB2ConnectionFactoryImpl extends ConnectionFactoryImpl {
 
-	public DB2ConnectionFactoryImpl() {
-		// TODO Auto-generated constructor stub
-	}
-
 	@Override
 	public DataSource createDataSource(Properties props) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		return createNativeDataSource(props);
 	}
 
 	@Override
-	public ConnectionPoolDataSource createConnectionPoolDataSource(
-			Properties props) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+	public ConnectionPoolDataSource createConnectionPoolDataSource(Properties props) throws SQLException {
+		return createNativeConnectionPoolDataSource(props);		
 	}
 
 	@Override
-	public XADataSource createXADataSource(Properties props)
-			throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+	public XADataSource createXADataSource(Properties props) throws SQLException {
+		throw new IllegalStateException("Not implemented yet");
 	}
 
 	@Override
 	public Driver createDriver(Properties props) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new IllegalStateException("Not implemented yet");
+	}
+
+	private DataSource createNativeDataSource(Properties props) {
+		try {
+			DB2Url db2Url = new DB2Url(props.getProperty(DataSourceFactory.JDBC_URL));
+			System.out.println("Connecting....");
+			DB2SimpleDataSource ds = new DB2SimpleDataSource();
+			ds.setRetrieveMessagesFromServerOnGetMessage(true);
+			ds.setServerName (db2Url.getHost());		
+			ds.setPortNumber(db2Url.getPort());
+			ds.setDatabaseName(db2Url.getDatabaseName());
+			ds.setUser(props.getProperty(DataSourceFactory.JDBC_USER));
+			ds.setPassword(props.getProperty(DataSourceFactory.JDBC_PASSWORD));
+			return ds;
+		} catch (Throwable e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	private ConnectionPoolDataSource createNativeConnectionPoolDataSource(Properties props) {
+		try {
+			DB2Url db2Url = new DB2Url(props.getProperty(DataSourceFactory.JDBC_URL));
+			System.out.println("Connecting....");
+			DB2ConnectionPoolDataSource ds = new DB2ConnectionPoolDataSource();
+			ds.setRetrieveMessagesFromServerOnGetMessage(true);
+			ds.setServerName (db2Url.getHost());		
+			ds.setPortNumber(db2Url.getPort());
+			ds.setDatabaseName(db2Url.getDatabaseName());
+			ds.setUser(props.getProperty(DataSourceFactory.JDBC_USER));
+			ds.setPassword(props.getProperty(DataSourceFactory.JDBC_PASSWORD));
+			return ds;
+		} catch (Throwable e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 }
--- org.asup.db.core.db2/src/org/asup/db/core/db2/T.java
+++
@@ -1,9 +0,0 @@
-package org.asup.db.core.db2;
-
-public class T {
-
-	public T() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
