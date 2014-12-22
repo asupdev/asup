3594edfb24d50c0dedec556b3b7df3f9f7ead64f
darioforesti <dario.foresti@smeup.com>
2014-12-22 09:52:41 +0100

    Issue #68

---
+++ org.asup.db.syntax.base/src/org/asup/db/syntax/base/BaseBindingParserRegistryImpl.java
@@ -0,0 +1,31 @@
+/**
+ *  Copyright (c) 2012, 2014 Sme.UP and others.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ * 
+ * Contributors: 
+ *   Mattia Rocchi - Initial API and implementation 
+ */
+package org.asup.db.syntax.base;
+
+import javax.inject.Inject;
+
+import org.asup.db.syntax.QBindingParser;
+import org.asup.db.syntax.impl.BindingParserRegistryImpl;
+import org.asup.fw.core.QPlugin;
+import org.asup.fw.core.QPluginRegistry;
+import org.asup.fw.core.QPluginRegistryFactory;
+
+public class BaseBindingParserRegistryImpl extends BindingParserRegistryImpl {
+
+	@SuppressWarnings("unchecked")
+	@Inject
+	public BaseBindingParserRegistryImpl(QPluginRegistryFactory pluginRegistryFactory) {
+		QPluginRegistry<? extends QPlugin> pluginRegistry = pluginRegistryFactory.createPluginRegistry(QBindingParser.class);
+		setStore((QPluginRegistry<QBindingParser>) pluginRegistry);
+	}
+
+}
