fc20d1d145154522202bae3dac5e9c1012a18ef4
darfores <darfores@ITERBNOT001.smeup.com>
2015-05-08 15:58:04 +0200

    Feature per parser plugins Nuovo plugin per creazione genmodel

---
+++ org.asup.ui.pdme.e4/src/org/asup/ui/pdme/e4/popup/actions/GenModelGeneratorAction.java
@@ -0,0 +1,44 @@
+package org.asup.ui.pdme.e4.popup.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+
+public class GenModelGeneratorAction implements IObjectActionDelegate {
+
+	private Shell shell;
+	
+	/**
+	 * Constructor for Action1.
+	 */
+	public GenModelGeneratorAction() {
+		super();
+	}
+
+	/**
+	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
+	 */
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		shell = targetPart.getSite().getShell();
+	}
+
+	/**
+	 * @see IActionDelegate#run(IAction)
+	 */
+	public void run(IAction action) {
+		MessageDialog.openInformation(
+			shell,
+			"As.UP",
+			"Create genmodel file was executed.");
+	}
+
+	/**
+	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+	}
+
+}
